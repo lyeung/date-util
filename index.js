941f708ac94684c151e788242ad44a1f549ad885
Lenming Yeung <lenming.yeung@gmail.com>
2021-04-29 00:38:19 +1000

    Fixed lint issue.

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const moment = require('moment');
 
 const DEFAULT_DATE_TIME_PATTERN = 'yyyy-MM-DD HH:mm:ss';
 
-const toUtc = (date, pattern = DEFAULT_DATE_TIME_PATTERN) => {
-  return moment.utc(date).format(pattern);
-};
+const toUtc = (date, pattern = DEFAULT_DATE_TIME_PATTERN) =>
+  moment.utc(date).format(pattern);
 
 const dateUtc = () => toUtc(new Date());
 
