a6803bff5cafc4cbb10edc49003c544fa38e2a55
Lenming Yeung <lenming.yeung@gmail.com>
2021-05-02 00:18:15 +1000

    Allow to sepcify date pattern.

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const moment = require('moment');
 
 const DEFAULT_DATE_TIME_PATTERN = 'yyyy-MM-DD HH:mm:ss';
 
+const toUtc = (date) => moment.utc(date);
+
 const toUtcStr = (date, pattern = DEFAULT_DATE_TIME_PATTERN) =>
-  moment.utc(date).format(pattern);
+  toUtc(date).format(pattern);
 
 const utcTime = (date) => {
-  const utcDate = moment.utc(date);
+  const utcDate = toUtc(date);
   const hourOfDay = utcDate.hour();
   const minuteOfHour = utcDate.minute();
   const secondOfMinute = utcDate.second();
@@ -17,6 +19,6 @@ const utcTime = (date) => {
   };
 };
 
-const dateUtcStr = () => toUtcStr(new Date());
+const dateUtcStr = (formatPattern) => toUtcStr(new Date(), formatPattern);
 
 module.exports = { dateUtcStr, toUtcStr, utcTime };
