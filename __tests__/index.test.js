e67d725eec41e96acf2578f790ce8cc5a2372d36
Lenming Yeung <lenming.yeung@gmail.com>
2021-05-01 22:52:26 +1000

    Fixed lint issues. Added get utc time component.

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,17 +1,35 @@
-const { toUtc } = require('../index');
+const { toUtcStr, utcTime } = require('../index');
 
 describe('index', () => {
-  describe('toUtc', () => {
+  describe('toUtcStr', () => {
     test('morning', () => {
       const date = new Date(Date.parse('01 Jan 2020 00:00:00 GMT+11'));
-      const result = toUtc(date);
+      const result = toUtcStr(date);
       expect(result).toEqual('2019-12-31 13:00:00');
     });
 
     test('evening', () => {
       const date = new Date(Date.parse('01 Jan 2020 23:00:00 GMT+11'));
-      const result = toUtc(date);
+      const result = toUtcStr(date);
       expect(result).toEqual('2020-01-01 12:00:00');
+    });
+  });
+
+  describe('utcTime', () => {
+    test('morning', () => {
+      const date = new Date(Date.parse('01 Jan 2020 10:11:12 GMT+11'));
+      const { hourOfDay, minuteOfHour, secondOfMinute } = utcTime(date);
+      expect(hourOfDay).toEqual(23);
+      expect(minuteOfHour).toEqual(11);
+      expect(secondOfMinute).toEqual(12);
+    });
+
+    test('evening', () => {
+      const date = new Date(Date.parse('01 Jan 2020 23:21:22 GMT+11'));
+      const { hourOfDay, minuteOfHour, secondOfMinute } = utcTime(date);
+      expect(hourOfDay).toEqual(12);
+      expect(minuteOfHour).toEqual(21);
+      expect(secondOfMinute).toEqual(22);
     });
   });
 });
--- index.js
+++ index.js
@@ -2,9 +2,21 @@ const moment = require('moment');
 
 const DEFAULT_DATE_TIME_PATTERN = 'yyyy-MM-DD HH:mm:ss';
 
-const toUtc = (date, pattern = DEFAULT_DATE_TIME_PATTERN) =>
+const toUtcStr = (date, pattern = DEFAULT_DATE_TIME_PATTERN) =>
   moment.utc(date).format(pattern);
 
-const dateUtc = () => toUtc(new Date());
+const utcTime = (date) => {
+  const utcDate = moment.utc(date);
+  const hourOfDay = utcDate.hour();
+  const minuteOfHour = utcDate.minute();
+  const secondOfMinute = utcDate.second();
+  return {
+    hourOfDay,
+    minuteOfHour,
+    secondOfMinute,
+  };
+};
 
-module.exports = { dateUtc, toUtc };
+const dateUtcStr = () => toUtcStr(new Date());
+
+module.exports = { dateUtcStr, toUtcStr, utcTime };
